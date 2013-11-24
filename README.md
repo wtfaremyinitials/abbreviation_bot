3e2d7b94737861bee41d25460ab05ecff9fd4231
Will Franzen <wtfranzen@gmail.com>
2013-11-24 17:18:10 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+A reddit bot that responds to comments with the phrase ’SINC’ in them with an explanation of what the abbreviation means.
+
+Created by [/u/wtf_are_my_initials](http://www.reddit.com/u/wtf_are_my_initials)
+Inspired by [this thread](http://www.reddit.com/r/gaming/comments/1rckxr/so_theyre_actually_charging_this_much_on_xbox_for/cdlvda3)
+
+Currently supported phrases
+===========================
+
+SINC - Strictly Informational, Not Confrontational
---
+++ bot.py
@@ -0,0 +1,38 @@
+import praw
+import string
+import time
+
+username = "abbreviation_bot"
+
+passfile = open('password.txt', 'r')
+password = passfile.read()[:-1]
+passfile.close()
+
+r = praw.Reddit(user_agent="bot created by /u/wtf_are_my_initials")
+r.login(username, password)
+sub = r.get_subreddit("all")
+
+already_replied = []
+
+response = "> SINC \n\nSINC stands for 'Simply Informational, Not Confrontational' \n\nI'm a friendly reddit bot that explains new or confusing abbreviations! You can find more information about me on [github](https://github.com/wtfaremyinitials/abbreviation_bot)!" # In the future I'll probably support multiple abbreviations, but this works for now.
+
+def reply(parent, message):
+	if(parent.id in already_replied): # Don't reply to the same comment twice
+		return
+	if(parent.author.name == username): # Don't talk to yourself
+		return
+	parent.reply(post)
+	already_replied.append(parent.id)
+
+def look_for_abbreviation(comment):
+	if(string.count(comment.body.lower(), " SINC") > 0): # The space before SINC is intentional to remove the possibility of SINC being found in a word
+		return True;
+
+def crawl():
+	for comment in r.get_comments():
+		if(look_for_abbreviatio(comment)):
+			reply(comment, response)
+			
+while(True):
+	crawl()
+	time.sleep(10)
